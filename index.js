54f00954fef5d110a6aa2f0ca9b6e622e00faf7a
grossinggames <grossinggames@gmail.com>
2019-03-02 16:12:13 +0300

    Little changes

--- index.js
+++ index.js
@@ -306,8 +306,8 @@ Blockly.JavaScript['play_video'] = function (block) {
         video: '',
         loop: true,
         next: false,
-        smoothStart: false,
-        smoothFinish: false,
+        smoothStart: true,
+        smoothFinish: true,
         open: function() {
             // PlaySound("sounds/sfx_.mp3", 0);
             // PlaySound("sounds/env_.mp3", 1);
